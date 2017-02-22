638d3cdef71dc4f1f3bf6aca699736b165c50c1e
Bill <storywr@gmail.com>
2017-02-22 00:04:25 -0500

    Done.

---
+++ components/Spaceship.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Spaceship extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <h1>{this.props.name}</h1>
+        <h2>{this.props.speed}</h2>
+        <p>Rockets: {this.props.hasRockets ? 'Yes' : 'No'}</p>
+        <p>Colors: {this.props.colors.join(', ')}</p>
+      </div>
+    );
+  }
+}
+
+Spaceship.defaultProps = {
+  hasRockets: false,
+  colors: ['black', 'red'],
+};
+
+module.exports = Spaceship;
