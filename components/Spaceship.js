8e1f7c154741abfd21edf47ece26be3da9a5d6fe
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-16 21:38:58 -0400

    Done.

---
+++ components/Spaceship.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Spaceship extends React.Component {
+  render () {
+    return (
+      <Spaceship propName="name">
+        
+
+      </Spaceship>
+
+    ) //return 
+  } // render
+} // class
+
+module.exports = Spaceship;
