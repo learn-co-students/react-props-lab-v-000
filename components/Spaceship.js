f6f0460a7253191782e8f297833abbd0a4edf7a1
yianna <yiannakokalas@gmail.com>
2017-05-18 12:44:03 -0400

    Yianna's Solution
    Create a Spaceship component to solve this lab that
    is intended to drive home the idea of using properties
    of a component.
    
    - Return properties of name, speed, hasRockets and colors.
    
    - Ensure that hasRockets and colors have default values
      in order to pass the tests.

---
+++ components/Spaceship.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Spaceship extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>{this.props.name}</li>
+        <li>{this.props.speed}</li>
+        <li>{this.props.hasRockets}</li>
+        <li>{this.props.colors}</li>
+      </ul>
+    )
+  }
+}
+
+Spaceship.defaultProps = {
+  hasRockets: false,
+  colors: ['black', 'red']
+}
+
+export default Spaceship
+
