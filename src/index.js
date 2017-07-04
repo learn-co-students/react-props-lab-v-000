a18f1a942cc6668d0923e108fd67c8fbb9373c09
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-07-04 11:09:03 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
+
+Spaceship.defaultProps = {
+  speed: "slow",
+  hasRockets: "false",
+  colors: ['black', 'red'].join(", ")
+
+}
+
+
+
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship name="Millennium Falcon"   />,
   document.getElementById('root')
 );
