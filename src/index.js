9bbbe8c838bbc91cf08bb78bfce0007b3f612f33
Moshe Levi <mo6709@gmail.com>
2017-10-29 12:19:04 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 import Spaceship from './components/Spaceship';
+
 
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" />,
