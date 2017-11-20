8a8d7e19778a2b0dacec668ef710195178544c68
alecgalba <alecgalba@gmail.com>
2017-11-20 11:21:00 -0500

    found an interesting pattern, unline the solutions and it worked

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,23 @@
 // Code The Spaceship Component Here
+import React from 'react';
+
+class Spaceship extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>Rockets? {this.props.hasRockets}</p>
+        <p>How fast? {this.props.speed}</p>
+        <p>Colors? {this.props.colors.join(', ')}</p>
+      </div>
+    )
+  }
+};
+
+Spaceship.defaultProps = {
+  speed: 'slow',
+  hasRockets: false,
+  colors: ['black', 'red']
+};
+
+export default Spaceship;
