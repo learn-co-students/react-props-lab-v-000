740890ca4c855feeeb4f8f390f9bc4387d280df4
Avi Flombaum <avi@flombaum.com>
2018-01-11 06:28:31 +0000

    Done.

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,1 +1,20 @@
 // Code The Spaceship Component Here
+class Spaceship extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <h1>Hello, {this.props.speed}</h1>
+        <h1>Hello, {this.props.hasRockets}</h1>
+      </div>
+    );
+  }
+}
+
+Spaceship.propTypes = {
+  name: PropTypes.string,
+  speed: "slow",
+  hasRockets: true
+};
+
+export default Spaceship
