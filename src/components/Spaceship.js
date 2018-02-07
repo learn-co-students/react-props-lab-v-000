5e7c51d45c73dd39edaa25fea5d5ee7fa6cb7f4c
Bob Habig <rfhab2@gmail.com>
2018-02-07 19:20:27 +0000

    Done.

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Spaceship extends React.Component {        render() {
-        return (
-            <div className="spaceship">
-                <h1>{this.props.name}</h1>
-                <h3><em>Speed: {this.props.speed}</em></h3>
-                <p>Does this thing have rockets? {this.props.hasRockets ? 
-                    "YOU BET YOUR ASS!" : 
-                    "yeah... that'd be cool, huh"}
-                </p>
-                <p><em>AND</em> it's {colorFormat(this.props.colors)}
-                </p>
-            </div>
-        )
-    }
+export default class Spaceship extends React.Component {
+  debugger;
+  render() {
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <h2>{typeof this.props.speed === 'number' ?
+          `${this.props.spee} MPH` :
+          this.props.speed
+        }</h2>
+      </div>
+    )
+  }
 }
 
 Spaceship.defaultProps = {
-    speed: 'slow',
-    hasRockets: false,
-    colors: ['black', 'red'],
+  speed: 'slow',
+  hasRockets: false,
+  colors: ["black", "red"]
 }
 
 Spaceship.propTypes = {
-    name: PropTypes.string.isRequired,
-    speed: PropTypes.string,
-    hasRockets: PropTypes.bool,
-    colors: PropTypes.arrayOf(PropTypes.string),
+  name: PropTypes.string.isRequired,
+  speed: PropTypes.number,
+  hasRockets: PropTypes.bool,
+  colors: PropTypes.arrayOf(PropTypes.string)
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import Spaceship from './components/Spaceship';
 
 ReactDOM.render(
-  <Spaceship name={"Millennium Falcon"} speed={200 + "mph"} hasRockets={true} />,
-  document.getElementById('root')
+  <Spaceship name="Millennium Falcon" />,
+  document.getElementById('global')
 );
