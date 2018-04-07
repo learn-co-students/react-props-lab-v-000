5dcababefb0c44c43f7e0c3115e7dcddecca557f
Igor Eskin <github email address>
2018-04-06 22:51:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import Spaceship from './components/Spaceship';
 
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship
+  name="Millennium Falcon"
+  speed="10,000 mph"
+  />,
   document.getElementById('global')
 );
