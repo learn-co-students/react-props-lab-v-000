5e2e2efa87b5d4a8e23d80a2c036c0691ad48a5e
smithwebtek <brad@smithwebtek.com>
2018-05-27 10:52:47 -0400

    Done.

--- src/components/Ships.js
+++ src/components/Ships.js
@@ -2,18 +2,25 @@ import React from 'react';
 import Spaceship from './Spaceship';
 
 class Ships extends React.Component {
-  render() {
+	render() {
 
-    const renderedShips = this.props.ships.map((ship, index) => {
-      return <Spaceship name={ship.name} speed={ship.speed} hasRockets={ship.hasRockets} colors={ship.colors} key={index} />
-    })
-    
-    return (
-      <div>
-        {renderedShips}
-      </div>
-    );
-  };
+		const renderedShips = this.props.ships.map((ship, index) => {
+			return (
+				<Spaceship
+					name={ship.name}
+					speed={ship.speed}
+					hasRockets={ship.hasRockets}
+					colors={ship.colors}
+					key={index} />
+			)
+		})
+
+		return (
+			<div>
+				{renderedShips}
+			</div>
+		);
+	};
 }
 
 export default Ships;
--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,28 +1,28 @@
 import React from 'react';
- 
+
 class Spaceship extends React.Component {
-  render() {
-    return (
-      <div className="spaceship">
-        <fieldset>
-          <h3>Name: {this.props.name}</h3>
-          <p>Speed: {this.props.speed}</p>
-          <p>Has rockets: {this.props.hasRockets || "false"}</p>
-          <p>Colors: {this.props.colors.join(', ')}</p>
-        </fieldset>
-      </div>
-    );
-  };
+	render() {
+		return (
+			<div className="spaceship">
+				<fieldset>
+					<h3>Name: {this.props.name}</h3>
+					<p>Speed: {this.props.speed}</p>
+					<p>Has rockets: {this.props.hasRockets ? 'true' : 'false'}</p>
+					<p>Colors: {this.props.colors.join(', ') || "no colors specified"}</p>
+				</fieldset>
+			</div>
+		);
+	};
 }
 
 Spaceship.defaultProps = {
-  colors: ['black', 'red'],
-  hasRockets: false,
-  speed: 'slow'
+	colors: ['black', 'red'],
+	hasRockets: false,
+	speed: 'slow'
 }
 
 Spaceship.propTypes = {
-  
+
 }
 
 export default Spaceship;
