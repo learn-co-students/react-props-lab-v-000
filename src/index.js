46c432b2b7dd5bacdcec5d1c9b7510a48c52fb45
jamiegiuliano <jamiepgiuliano@gmail.com>
2018-07-25 13:34:44 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 
 import Spaceship from './components/Spaceship';
 
+const millenniumColors = ['blue', 'silver', 'black']
+
 ReactDOM.render(
-  <Spaceship name="Millennium Falcon" />,
+  <Spaceship
+    name="Millennium Falcon"
+    speed="Crazy Fast"
+    colors={millenniumColors}
+    />,
   document.getElementById('global')
 );
